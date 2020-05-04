e718eca4037b119a568d69396afd359fe4e4df03
lifanyuan <1030426634@qq.com>
2020-05-04 21:13:54 +0800

    加入core.py，改变神经网络节点数

--- examples/dqn_cartpole.py
+++ examples/dqn_cartpole.py
@@ -22,11 +22,11 @@ nb_actions = env.action_space.n
 # Next, we build a very simple model.
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(16))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dense(16))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dense(16))
+model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
@@ -45,8 +45,8 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Ctrl + C.
 dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
 
-# After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
-
+# # After training is done, we save the final weights.
+# dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+# dqn.load_weights('dqn_{}_weights.h5f'.format(ENV_NAME))
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=False)
+dqn.test(env, nb_episodes=20, visualize=False)
